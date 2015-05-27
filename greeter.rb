930ca5be7a69437b9de7115c829068bfc7344cb1
Brick Thornton <brickthornton@gmail.com>
2015-05-27 15:09:29 -0700

    view refactored

--- greeter.rb
+++ greeter.rb
@@ -1,15 +1,36 @@
-puts "Welcome to the Greetinator"
 
-print "Enter in your name: "
-name = gets.chomp
+module View
+
+  def self.welcome
+    puts "Welcome to the Greetinator"
+  end
+
+  def self.get_name
+    print "Enter in your name: "
+    gets.chomp
+  end
+
+  def self.familiar_greeting(name)
+    puts "Hello #{name}. I've heard that name before."
+  end
+
+  def self.unfamiliar_greeting(name)
+    puts "#{name}?!?  Is that your real name?!?"
+    puts "We won't forget it!"
+  end
+
+end
+
+
+View.welcome
+name = View.get_name
 
 normal_names = File.readlines("normal_names.txt")
 
 if normal_names.include?(name.capitalize + "\n")
-  puts "Hello #{name}"
+  View.familiar_greeting(name)
 else
-  puts "#{name}?!?  Is that your real name?!?"
+  View.unfamiliar_greeting(name)
   File.open("normal_names.txt", "a") { |f| f.write(name.capitalize + "\n") }
-  puts "We won't forget it!"
 end
 
